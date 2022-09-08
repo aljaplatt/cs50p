e0bed0c35998e9d52ace9d5c6ab6582e337d5634
Alex <alja.platt@gmail.com>
2022-09-08 11:37:11 +0100

    meal.py

--- problem_set_1/meal.py
+++ problem_set_1/meal.py
@@ -1,13 +1,22 @@
 def main():
     time = input("Please enter what time it is in a 24hr format eg 13:00: ").strip()
 
+    # converted_time = convert(time)
+    # print(converted_time)
+    # if converted_time >= 7 and converted_time <= 8:
+    #     print('breakfast time')
+    # elif converted_time >= 12 and converted_time <= 13:
+    #     print('lunch time')
+    # elif converted_time >= 18 and converted_time <= 19:
+    #     print('dinner time')
+
     converted_time = convert(time)
     # print(converted_time)
-    if converted_time >= 7 and converted_time <= 8:
+    if 7 <= converted_time <= 8:
         print('breakfast time')
-    elif converted_time >= 12 and converted_time <= 13:
+    elif 12 <= converted_time <= 13:
         print('lunch time')
-    elif converted_time >= 18 and converted_time <= 19:
+    elif 18 <= converted_time <= 19:
         print('dinner time')
 
 

f6ed0fcf099ad4a8cfd7e6a0cf966e2f77385695
Alex <alja.platt@gmail.com>
2022-09-08 17:49:42 +0100

    looping lists

--- problem_set_2_loops/src2/hogwarts0.py
+++ problem_set_2_loops/src2/hogwarts0.py
@@ -2,6 +2,45 @@
 
 students = ["Hermione", "Harry", "Ron"]
 
-print(students[0])
-print(students[1])
-print(students[2])
+# print(students[0])
+# print(students[1])
+# print(students[2])
+# print(students[-1])
+
+#* 1  
+for index in range(len(students)):
+    print(index) # 0,1,2
+
+#* 2
+for index in range(1, len(students) + 1):
+    print(index) # 1,2,3
+
+#* 3
+for i in range(len(students)):
+    print('STUDENTS[I]: ', students[i])
+
+'''
+Hermione
+Harry
+Ron
+'''
+
+#* 4
+for i in range(len(students)):
+    print('STUDENTS[I]: ', i + 1, students[i])
+
+'''
+1 Hermione
+2 Harry
+3 Ron
+'''
+
+#* 5
+for student in students:
+    print(student)
+
+'''
+Hermione
+Harry
+Ron
+'''

097d02fb957a939fead1fa657c129ba76ecd2edf
Alex <alja.platt@gmail.com>
2022-09-12 17:13:30 +0100

    sys.argv

--- problem_set_4_libraries/src4/name0.py
+++ problem_set_4_libraries/src4/name0.py
@@ -2,4 +2,15 @@
 
 import sys
 
-print("hello, my name is", sys.argv[1])
+# print("hello, my name is", sys.argv[1])
+
+# have to run with 'python name0.py Alex' from correct folder
+name = sys.argv[1]
+print(f"hello, my name is {name}")
+
+# hello, my name is Alex
+
+# relative path - problem_set_4_libraries/src4/name0.py
+
+
+

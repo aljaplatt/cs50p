e36e72cf63adc4d2019818ccc1f10f61c8300e2c
Alex <alja.platt@gmail.com>
2022-09-14 13:52:58 +0100

    testing shorten

--- problem_set_5_unit_tests/twttr.py
+++ problem_set_5_unit_tests/twttr.py
@@ -1,10 +1,31 @@
 def main():
-    ...
+    user_str = input("Input: ")
+    print(shorten(user_str))
 
 
 def shorten(word):
-    ...
+    VOWELS = ("a", 'e', 'i', 'o', 'u')
+
+    new_string = ''
+    for letter in word:
+        if letter.lower() not in VOWELS:
+            new_string += letter
+
+    return new_string
 
 
 if __name__ == "__main__":
     main()
+
+
+# user_string = input("Input: ")
+
+# VOWELS = ("a", 'e', 'i', 'o', 'u')
+
+# new_string = ''
+
+# for letter in user_string:
+#     if letter.lower() not in VOWELS:
+#         new_string += letter
+
+# print(new_string)

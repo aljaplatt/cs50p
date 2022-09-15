92030486eda4153bf10a2434333ee690616d0314
Alex <alja.platt@gmail.com>
2022-09-15 14:31:41 +0100

    pizza file io

--- problem_set_6_file_i_o/pizza.py
+++ problem_set_6_file_i_o/pizza.py
@@ -3,29 +3,26 @@ import csv
 from tabulate import tabulate
 
 def main():
-    check_command_line_arg()
-    # Create variable to store the table data
-    table = []
-    # Try to open the file
+    check_args()
     try:
-        with open(sys.argv[1]) as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                table.append(row)
-    # If can't open this means that the file does not exist
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-    print(tabulate(table[1:], table[0], tablefmt="grid"))
+        with open(sys.argv[1]) as file:
+            reader = csv.DictReader(file)
+            print(tabulate(reader, headers="keys", tablefmt="grid"))
 
-def check_command_line_arg():
-    #Check how many elements in the command line
+    except FileNotFoundError:
+        sys.exit("File not found")
+
+def check_args():
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
-    # Check if it is a CSV file
+
     if ".csv" not in sys.argv[1]:
         sys.exit("Not a CSV file")
 
 if __name__ == "__main__":
     main()
+
+
+# python pizza.py regular.csv

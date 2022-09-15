ff7d0ce71068660fd7fb1abbdd62f5629a3ad378
Alex <alja.platt@gmail.com>
2022-09-15 11:53:32 +0100

    tests complete

---
+++ problem_set_5_unit_tests/test_fuel.py
@@ -0,0 +1,19 @@
+from fuel import convert, gauge
+import pytest
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert('1/0')
+
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+
+
+def test_correct_output():
+    assert convert('1/4') == 25 and gauge(25) == '25%'
+    assert convert('1/100') == 1 and gauge(1) == 'E'
+    assert convert('99/100') == 99 and gauge(99) == 'F'
+

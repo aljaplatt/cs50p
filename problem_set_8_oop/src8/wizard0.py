0df6bdab2ddba51887e455915a6e1b2dcc249e98
Alex <alja.platt@gmail.com>
2022-09-21 13:26:23 +0100

    inheritance

---
+++ problem_set_8_oop/src8/wizard0.py
@@ -0,0 +1,16 @@
+
+# Lots of repetition in name etc. We could use inheritance to reduce this.
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+
+
+class Professor:
+    def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.subject = subject 
